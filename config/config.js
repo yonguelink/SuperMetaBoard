ab929af3c428ef70f31385241dffc8e65d17805d
Isaac Ouellet Therrien <yonguelink@users.noreply.github.com>
2015-03-05 13:46:50 -0500

    Create a default config file

---
+++ config/config.js
@@ -0,0 +1,5 @@
+/*This file contains the configuration for the project*/
+/*The variable defaultLoad should be an array 
+  With the kind of item we want to load in the first position (state or board)
+  and the name of the item we want to load (case sensitive) in the second position*/
+var defaultLoad = ['state','Ready for Sprint Review'];
